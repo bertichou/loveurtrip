eb19baec3a415d69607f352801b075fd517693c8
Bertichou <mikel757@outlook.fr>
2017-08-16 17:30:18 +0000

    Amélioration de notre formulaire

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,13 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show]
   resources :rooms do
-    resources :reservations, only: [:create]
-  end 
+ 
+            resources :reservations, only: [:create]
+ 
+   end
   
-  
+get '/preload' => 'reservations#preload'
+get '/preview' => 'reservations#preview'
   
   
   resources :photos
