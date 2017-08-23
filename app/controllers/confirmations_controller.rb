bb9b648f52632e6df6fd43b3f42084c99c3b38d7
Bertichou <mikel757@outlook.fr>
2017-08-23 20:42:19 +0000

    Modification devise pour confirmation mail

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,12 @@
+class ConfirmationsController < Devise::ConfirmationsController
+ 
+  
+Private
+ 
+     def after_confirmation_path_for(resource_name, resource)
+ 
+          edit_user_registration_path
+ 
+     end
+ 
+ en
