9ab74f4e464b89db0323e4a34cbef111a55961cb
Bertichou <mikel757@outlook.fr>
2017-08-23 14:26:50 +0000

    Amélioration messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,7 +5,7 @@ class Conversation < ActiveRecord::Base
  
       belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
 
-      has_many :message, dependent: :destroy 
+      has_many :messages, dependent: :destroy 
       
       validates_uniqueness_of :sender_id, scope: :recipient_id
       
